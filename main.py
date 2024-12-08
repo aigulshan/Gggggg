4338e244a0611de9b8142acdccbb1333b841d7c8
aigulshan <factmga@gmail.com>
2024-12-08 15:59:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '8034049669:AAHPwwaFW9MVAW9ZlCDWGMRze9-S58w0utI'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
